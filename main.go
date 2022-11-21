44daa848aa067243a0da9219c139160ba8c004e4
Sergey Khliustin <sergey.khliustin@gmail.com>
2022-11-21 21:43:17 +0100

    Added timestamp option

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/creack/pty"
 	"io"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
 	logger := log.NewLogger()
-	logger.EnableDebugLog(true)
-	logger.Debugf("Started")
 	runner := os.Getenv("runner_bin")
 	content := os.Getenv("content")
+	tmpDir := os.Getenv("TMPDIR")
+	scriptPath := tmpDir + "/._script_cont"
+	workingDir := os.Getenv("working_dir")
+	scriptFilePath := os.Getenv("script_file_path")
+	isDebug := os.Getenv("is_debug")
+	logger.EnableDebugLog(isDebug == "yes")
+	timestampEnabled := os.Getenv("timestamp") == "yes"
+
+	logger.Debugf("==> Start")
 
 	if runner == "" {
 		logger.Errorf("runner_bin is empty")
 		os.Exit(1)
 	}
 
-	f, err := os.Create("._script_cont")
+	if workingDir != "" {
+		err := os.Chdir(workingDir)
+		if err != nil {
+			logger.Errorf(" [!] Failed to switch to working directory: %s", workingDir)
+		}
+	}
+
+	if scriptFilePath != "" {
+		logger.Debugf("==> Script (tmp) save path specified: %s", scriptFilePath)
+		scriptPath = scriptFilePath
+	}
+
+	f, err := os.Create(scriptPath)
 
 	if err != nil {
 		logger.Errorf(err.Error())
@@ -35,30 +57,41 @@ func main() {
 	}
 	_ = f.Close()
 
-	fmt.Printf("Content %s", content)
-
-	_, err = exec.LookPath(runner)
-
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	cmd := exec.Command(runner, "._script_cont")
+	runnerArgs := strings.Fields(runner)
+	runnerBin := runnerArgs[0]
+	args := runnerArgs[1:]
+	args = append(args, scriptPath)
+
+	cmd := exec.Command(runnerBin, args...)
 	file, err := pty.Start(cmd)
 	defer func() { _ = file.Close() }()
-	logger.Debugf("Start copy bytes")
-
-	_, copyErr := io.Copy(os.Stdout, file)
-	logger.Debugf("End copy bytes")
+	logger.Debugf("Start logging")
+	if timestampEnabled {
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			timestamp := []byte(time.Now().Format("15:04:05") + " ")
+			text := append(scanner.Bytes(), []byte("\n")...)
+			_, _ = os.Stdout.Write(append(timestamp, text...))
+		}
+	} else {
+		_, copyErr := io.Copy(os.Stdout, file)
+		if copyErr != nil {
+			logger.Errorf(err.Error())
+			os.Exit(1)
+		}
+	}
+	logger.Debugf("End end logging")
 	if err != nil {
 		logger.Errorf(err.Error())
 		os.Exit(1)
 	}
-	if copyErr != nil {
-		logger.Errorf(err.Error())
-		os.Exit(1)
-	}
+
 	if err != nil {
 		logger.Errorf(err.Error())
 		os.Exit(1)
@@ -68,4 +101,5 @@ func main() {
 		logger.Errorf(err.Error())
 		os.Exit(1)
 	}
+	os.Exit(0)
 }
